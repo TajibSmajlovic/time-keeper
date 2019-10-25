b0ead996a260347e563a8a0fcc8d2f8f7d68db9a
TajibSmajlovic <smajlovic.tajib@gmail.com>
2019-10-25 11:29:44 +0200

    Updates

---
+++ precache-manifest.f9bc2694df011931ee31ccedd7f3b19e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8628d661f3897145321ace2addb0df42",
+    "url": "/time-keeper/index.html"
+  },
+  {
+    "revision": "c517aa78b6963b9f2adc",
+    "url": "/time-keeper/static/css/2.3f1b02eb.chunk.css"
+  },
+  {
+    "revision": "a9bc6b1538e45c308690",
+    "url": "/time-keeper/static/css/main.d8283262.chunk.css"
+  },
+  {
+    "revision": "c517aa78b6963b9f2adc",
+    "url": "/time-keeper/static/js/2.4585575e.chunk.js"
+  },
+  {
+    "revision": "a9bc6b1538e45c308690",
+    "url": "/time-keeper/static/js/main.400046f2.chunk.js"
+  },
+  {
+    "revision": "aca1992bb2767db1bea6",
+    "url": "/time-keeper/static/js/runtime-main.60c9513e.js"
+  },
+  {
+    "revision": "0482db8f34b636e5effe73efbb4b7f3a",
+    "url": "/time-keeper/static/media/Ajdin Zorlak.0482db8f.jpg"
+  },
+  {
+    "revision": "569af6039504469d49a53e165a05fa08",
+    "url": "/time-keeper/static/media/Amila Šikalo.569af603.jpg"
+  },
+  {
+    "revision": "ea012e95cb3bc982bb91af1c16f24ecc",
+    "url": "/time-keeper/static/media/Armin Odobašić.ea012e95.jpg"
+  },
+  {
+    "revision": "eec97fad543036ecb4ff3b8b659e7315",
+    "url": "/time-keeper/static/media/Merima Kovačević.eec97fad.jpg"
+  },
+  {
+    "revision": "01b548b2f01fc6722222ec75a863f959",
+    "url": "/time-keeper/static/media/Nađa Šišić.01b548b2.jpg"
+  },
+  {
+    "revision": "89e13aef2d40593b1f1d15be90697540",
+    "url": "/time-keeper/static/media/Tajib Smajlović.89e13aef.jpg"
+  },
+  {
+    "revision": "4553970504e6e8f55d2875dfd6c3509e",
+    "url": "/time-keeper/static/media/backgundWithEffect.45539705.png"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/time-keeper/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/time-keeper/static/media/github-sign.8f8edc5f.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/time-keeper/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/time-keeper/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/time-keeper/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/time-keeper/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/time-keeper/static/media/slick.f97e3bbf.svg"
+  }
+]);
